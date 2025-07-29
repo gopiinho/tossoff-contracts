a9f2a97eae937688cf5a751ccf1315aef251b069
Gurpreet <gs109690@gmail.com>
2025-07-29 13:53:56 +0530

    test: add testCanCreateMatch

--- test/Engine.t.sol
+++ test/Engine.t.sol
@@ -7,15 +7,37 @@ import {Engine} from "../src/Engine.sol";
 contract EngineTest is Test {
     Engine public engine;
     address public owner;
+    address public user;
+    uint256 public betAmount = 0.5 ether;
 
     uint8 public constant FEE = 3;
     address public constant VRF = 0xC04ae87CDd258994614f7fFB8506e69B7Fd8CF1D;
 
     function setUp() public {
         owner = makeAddr("OWNER");
+        user = makeAddr("USER");
+
+        deal(user, 10 ether);
 
         vm.startPrank(owner);
         engine = new Engine(FEE, VRF);
         vm.stopPrank();
+    }
+
+    modifier createMatch() {
+        vm.startPrank(user);
+        engine.createMatch{value: betAmount}();
+        vm.stopPrank();
+        _;
+    }
+
+    function testCanCreateMatch() public {
+        vm.startPrank(user);
+        uint256 matchId = engine.createMatch{value: betAmount}();
+        vm.stopPrank();
+
+        Engine.Match memory gameMatch = engine.getMatch(matchId);
+        assertEq(gameMatch.amount, betAmount);
+        assertEq(gameMatch.player1, user);
     }
 }
